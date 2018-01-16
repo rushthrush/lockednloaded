f11a52df9a7c72a02c0e3017e97f94645b543f64
rushthrush <robin@flintriverhills.com>
2018-01-15 20:14:20 -0500

    finally working

--- lockednloaded/Views/PlaceListWebViewPage.cs
+++ lockednloaded/Views/PlaceListWebViewPage.cs
@@ -4,11 +4,11 @@ using LockedNLoaded.Models;
 namespace LockedNLoaded.Views
 {
     // [START custom_web_view]
-    public abstract class PlacesLibraryWebViewPage<TModel> : WebViewPage<TModel>
+    public abstract class PlaceListWebViewPage<TModel> : WebViewPage<TModel>
     {
         public User CurrentUser => new User(this.User);
     }
     // [END custom_web_view]
 
-    public abstract class PlacesLibraryWebViewPage : WebViewPage { }
+    public abstract class PlaceListWebViewPage : WebViewPage { }
 }
